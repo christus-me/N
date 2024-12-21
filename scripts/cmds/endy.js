d94a88d91f1abefe4596f0366029f0a1d5d268b2
101danny <160006806+101danny@users.noreply.github.com>
2024-12-21 13:15:27 +0100

    Add files via upload

---
+++ scripts/cmds/endy.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const fs = require("fs");
+
+module.exports = {
+  config: {
+    name: "Endy",
+    version: "1.0",
+    author: "AceGun",
+    countDown: 5,
+    role: 0,
+    shortDescription: "no prefix",
+    longDescription: "no prefix",
+    category: "no prefix",
+  },
+
+  onStart: async function() {},
+
+  onChat: async function({ event, message, api, getLang }) {
+    if (event.body && event.body.toLowerCase().includes("owner")) {
+      try {
+        // Set reaction to the message
+        api.setMessageReaction("💋", event.messageID, () => {}, true);
+
+        // Define the Imgur video link
+        const videoUrl = "https://i.imgur.com/TCuQx0p.mp4";
+
+        // Download the video from the Imgur link
+        const response = await axios({
+          url: videoUrl,
+          method: "GET",
+          responseType: "stream",
+        });
+
+        // Send the reply with the video
+        return message.reply({
+          body: "Hi, I am Endy. Developed by Dàññy Çodex💔.🤍🌌",
+          attachment: response.data,
+        });
+      } catch (error) {
+        console.error("Error setting reaction or sending reply:", error);
+      }
+    }
+  },
+};
